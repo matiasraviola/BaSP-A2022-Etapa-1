118170ff2a418e546661fdbeb440d2db1012ed4f
matiasraviola <matiasaraviola@gmail.com>
2022-09-24 17:48:56 -0300

    BaSP-W06-2022: button alerts almost done

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -8,17 +8,19 @@ window.onload = function(){
     alert2.innerHTML = 'Invalid Email';
     var showPass = document.getElementById('show-password');
     var button = document.getElementById('button-login');
-
+    var arrayInput = [];
 
     // VALIDACION DEL EMAIL
-    email.onblur = function emailOb (){
+    email.onblur = function (){
         if (!emailExpression.test(email.value)){
             email.classList.add("red-border");
             email.parentNode.insertBefore(alert2, email.nextElementSibling);
+            arrayInput[0] = 'Invalid Email';
             return false;
         }else if (email.value == ''){
             email.classList.add("red-border");
             email.parentNode.insertBefore(alert2, email.nextElementSibling);
+            arrayInput[0] = 'No spaces alowed!';
             return false;
         }else {
             email.classList.add("green-border");
@@ -32,18 +34,17 @@ window.onload = function(){
         email.parentNode.removeChild(alert2);
     }
     // Validation PW
-    password.onblur = function passwordOb (){
-        // var espacios = false;
-        // var cont = 0;
-        //Validation: > 8
+    password.onblur = function (){
         if (password.value.length < 8){
             password.classList.add("red-border");
             password.parentNode.insertBefore(alert1, password.nextElementSibling);
+            arrayInput[1] = 'Need more characters';
             return false;
 
         }else if (!numberValidation(password.value)){
             password.classList.add("red-border");
             password.parentNode.insertBefore(alert1, password.nextElementSibling);
+            arrayInput[1] = 'Need numbers';
             return false;
         }
         else{
@@ -57,7 +58,6 @@ window.onload = function(){
         password.classList.remove("red-border");
         password.parentNode.removeChild(alert1);
     }
-
     function numberValidation (stringNumber){
         var numbers = ['0','1','2','3','4','5','6','7','8','9'];
         for (i=0; i < stringNumber.length; i++){
@@ -66,16 +66,44 @@ window.onload = function(){
             }
         }
     }
+
+    // function lettersValidation (letterString){
+    //     var AZ = letterString.toUpperCase().charCodeAt(0);
+    //     // return ascii > 64 && ascii <91;
+    //     if ( AZ > 64 && AZ <91){
+    //         return true;
+    //         } return false;
+    // }
+    // function Letter(letterString){
+    //     var Ascii = letterString.charCodeAt(0);
+    //     // return ascii > 64 && ascii <91;
+    //     if ( (Ascii > 64 && Ascii <91)){
+    //         return true;
+    //     } else if((AZ > 96 && AZ < 123)){
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    // function isChars(myString) {
+    //     for (var x = 0 ; x < myString.length; x++) {
+    //       if (!((myString[x] >= 'a' && myString[x] <= 'z') || (myString[x] >= 'A' && myString[x] <= 'Z'))) {
+    //         return false;
+    //       }
+    //     }
+    //     return true;
+    // }
     showPass.onclick = function(){
-        password.type='text'
+        password.type='text';
     }
     button.onclick = function(){
-        var resultPass = passswordOb ();
-        var resultEmail= emailOb();
+        var resultPass = password.onblur (password.value);
+        var resultEmail= email.onblur(email.value);
         if (resultPass == true && resultEmail == true){
-            alert('')
+            alert('Welcome to Trackgenix');
+        }else{
+            alert('Something goes wrong: '+ arrayInput[0]+' '+ arrayInput[1]);
         }
     }
-
     console.log('hola');
 }
